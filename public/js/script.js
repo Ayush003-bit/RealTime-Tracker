60e05af27a317f42b0bf12efec66ff270bdc2d5c
Ayush Singh <raghukulayush003@gmail.com>
2025-07-05 16:13:51 +0530

    Changes in attribution in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -23,7 +23,7 @@
   const map =  L.map("map").setView([0, 0], 10);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-         attribution: "Sheryians Coding School"
+         attribution: "raghukulayush"
     }).addTo(map);
 
     const markers = {};
